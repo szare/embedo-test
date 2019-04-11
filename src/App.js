8d1aba07e013ad51e69ce2cbb10a0baacd854912
Salman Zare <salman.zare@optus.com.au>
2019-04-11 16:40:58 +1000

    adding the embo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EmbedoItem from './EmbedoItem';
 
 class App extends Component {
   render() {
+    const lists = [
+      {
+        "id": "85ac034e-318d-44fc-b793-ddef82d31bb1",
+        "type": "SOCIAL",
+        "value": "https://twitter.com/Dan_Kiko_Murray/status/1116149285081341953",
+        "position": 14,
+        "media": null
+      },
+      {
+        "id": "f094439a-17b3-4cac-a47a-526c8ad931db",
+        "type": "SOCIAL",
+        "value": "https://www.facebook.com/actually.free/photos/rpp.212877235395354/724537360896003/?type=3&theater",
+        "position": 15,
+        "media": null
+      },
+      {
+        "id": "736d4335-159f-44fb-8423-28d7f955dde1",
+        "type": "SOCIAL",
+        "value": "https://www.pinterest.com.au/pin/451556300114351400",
+        "position": 16,
+        "media": null
+      },
+      {
+        "id": "2fd30572-3e98-47c2-bc47-e11515ac070f",
+        "type": "SOCIAL",
+        "value": "https://www.instagram.com/p/ButHiDZFE34/",
+        "position": 13,
+        "media": null
+      },
+    ];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {lists.map(list => {
+          return <EmbedoItem
+            key={list.id}
+            id={list.id}
+            href={list.value}
+          // name={content.name || ''}
+          // data-width={'100%'}
+          // data-height={html.data.options.height}
+          // data-options={html.data.options ? html.data.options : {}}
+          />
+        })}
       </div>
     );
   }
---
+++ src/EmbedoItem.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Embedo from 'embedo';
+
+const embedo = new Embedo({
+    twitter: true,
+    instagram: true,
+    pinterest: true,
+    facebook: {
+        appId: '353165765519639',
+        version: 'v3.2',
+        xfbml: true
+    },
+    youtube: true
+});
+
+export default class EmbedoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false
+        };
+    }
+
+    render() {
+        return (
+            <div className='embedoWrapper'>
+                <a
+                    className=""
+                    key={this.props.id}
+                    href={this.props.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    ref={c => (this.container = c)}
+                    id={this.props.id}
+                >
+                    {/* <h1>{this.props.name}</h1> */}
+                </a>
+            </div>
+        );
+    }
+
+
+    componentDidMount() {
+        let self = this;
+        // let options = this.props["data-options"] || {};
+        // let width = this.props["data-width"];
+        // let height = this.props["data-height"];
+
+        // if (width) {
+        // 	options.width = width;
+        // }
+
+        // if (height) {
+        // 	options.height = height;
+        // }
+        let options = {};
+        options.maxwidth = 800;
+        options.width = 800;
+
+        embedo
+            .load(document.getElementById(this.props.id),
+                this.props.href, options)
+            .done((data) => {
+                self.setState({
+                    loaded: true
+                });
+            })
+            .fail((err) => {
+            });
+    }
+}
